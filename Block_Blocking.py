56ff086ead5d7328410b44201dcf42ce71635115
Rodrigo Ansa <rodrigoansa2@gmail.com>
2023-01-27 07:29:19 -0300

    Movendo principal

--- Block_Blocking.py
+++ Block_Blocking.py
@@ -24,7 +24,7 @@ play = pygame.image.load('imagens/play.png')
 retorno = pygame.image.load('imagens/return.png')
 suporte = pygame.image.load('imagens/suporte.png')
 bg = pygame.image.load('imagens/bg.png')
-acertos = font.render(contagem, True, (175, 175, 175))
+acertos = font.render('2', True, (175, 175, 175))
 # bg = pygame.draw.rect(tela, cinza, (0, 0, 540, 960))
 
 
@@ -39,12 +39,17 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
-        if event.type == KEYDOWN:
-            if event.key == K_LEFT:
-                pos_play = pos_play - 10  
+        # if event.type == KEYDOWN:
+        #     if event.key == K_LEFT:
+        #         pos_play = pos_play - 10  
                                          
-            if event.key == K_RIGHT:
-                pos_play = pos_play + 10
+        #     if event.key == K_RIGHT:
+        #         pos_play = pos_play + 10
+    
+    if pygame.key.get_pressed()[K_LEFT]:
+        pos_play = pos_play - 5
+    if pygame.key.get_pressed()[K_RIGHT]:
+        pos_play = pos_play + 5
                 
     
 
