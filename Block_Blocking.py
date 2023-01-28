72c0474bef0871ba598899c10ff8eb742d383f13
Rodrigo Ansa <rodrigoansa2@gmail.com>
2023-01-27 21:01:56 -0300

    Inicio da funcionalidade do jogo

--- Block_Blocking.py
+++ Block_Blocking.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randrange
 from pygame.locals import *
 from sys import exit
 
@@ -6,8 +7,8 @@ pygame.init()
 
 largura = 540
 altura = 960
-pos_play = 250
 contagem = 0
+
 cinza = pygame.Color(226, 226, 226)
 font = pygame.font.SysFont('fixtureexpandedmedium', 200)
 click = False
@@ -24,13 +25,14 @@ marca = pygame.image.load('imagens/marca.png')
 play = pygame.image.load('imagens/play.png')
 retorno = pygame.image.load('imagens/return.png')
 suporte = pygame.image.load('imagens/suporte.png')
+
 bg = pygame.image.load('imagens/bg.png')
-acertos = font.render('2', True, (175, 175, 175))
+acertos = font.render(str(contagem), True, (175, 175, 175))
 
 
 def menu():
-    while True:
-        butao = pygame.Rect(50, 100, 200, 50)
+    while True:        
+        butao = pygame.Rect(190, 688, 150, 75)
         tela.blit(bg, (0, 0))
         tela.blit(marca, (75, 300))
         # tela.blit(play, (250, 700))
@@ -38,7 +40,8 @@ def menu():
         if butao.collidepoint((mx, my)):
             if click:
                 game()
-        pygame.draw.rect(tela, (255, 0, 0), butao)
+        pygame.draw.rect(tela, (175, 175, 175), butao)
+        tela.blit(play, (245, 700))
         click = False
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -58,11 +61,21 @@ def menu():
 
 
 def game():
-    while True:
+    rot_ret = 0
+    pos_x = 50
+    pos_y = -25
+    ret_preto = pygame.transform.rotate(pygame.image.load('imagens/ret_preto.png'), rot_ret)
+    
+    # ret_preto = pygame.Rect(pos_x, pos_y, 50, 50)
+    
+    pos_play = 250
+    running = True
+    while running:
         tela.blit(bg, (0, 0))
         tela.blit(suporte, (20, 480))
-        tela.blit(jogador, (pos_play, 480))
+        tela.blit(jogador, (pos_play, 480))        
         tela.blit(acertos, (185, 600))
+        
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -71,12 +84,23 @@ def game():
         if pygame.key.get_pressed()[K_LEFT]:
             pos_play = pos_play - 5
             if pos_play <= 20:
-                pos_play = 20        
+                pos_play = 20                      
 
         if pygame.key.get_pressed()[K_RIGHT]:
             pos_play = pos_play + 5
             if pos_play >= 450:
-                pos_play = 450         
+                pos_play = 450  
+
+        
+        tela.blit(ret_preto, (pos_x, pos_y))
+        if pos_y >= altura:
+            pos_y = -25
+            pos_x = randrange(40, 500, 100)
+        pos_y = pos_y + 3
+        rot_ret = rot_ret + 3
+        
+        
+                       
         
         pygame.display.flip()
 
